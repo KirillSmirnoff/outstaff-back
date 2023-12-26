eebcf111998637ffb0eedd6f84a66548db66792c
Kirill Smirnov <loha8088@gmail.com>
2023-12-26 23:40:25 +0300

    какой то бардак тупо нужно закомитить все и начать исправлять

--- src/main/kotlin/ru/k2/outstaff/persistence/CompanyRepository.kt
+++ src/main/kotlin/ru/k2/outstaff/persistence/CompanyRepository.kt
@@ -4,12 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
-import ru.k2.outstaff.persistence.entity.CompanyEntity
+import ru.k2.outstaff.persistence.entity.Company
 
 @Repository
-interface CompanyRepository : JpaRepository<CompanyEntity, Int>{
+interface CompanyRepository : JpaRepository<Company, Int>{
 
-    @Query("select c from CompanyEntity c where c.companyName = :companyName")
-    fun findByName(@Param("companyName") companyName: String): CompanyEntity
+    @Query("select c from Company c where c.companyName = :companyName")
+    fun findByName(@Param("companyName") companyName: String): Company
 
 }
